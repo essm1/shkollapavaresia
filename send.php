9a26b2ab6cb8abd7060470b295d2a21dc03c7a1c
essm1 <essma.mustafa1@gmail.com>
2026-06-08 17:25:57 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,7 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $emri = $_POST['emri'];
+    $mbiemri = $_POST['mbiemri'];
     $email = $_POST['email'];
     $telefoni = $_POST['telefoni'];
     $mesazhi = $_POST['mesazhi'];
@@ -8,7 +9,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Vendos emailin e drejtuesit të shkollës këtu
     $to = "drejtuesi@gmail.com";  
     $subject = "Mesazh nga formulari i faqes";
-    $body = "Emri: $emri\nEmail: $email\nTel: $telefoni\nMesazhi: $mesazhi";
+    $body = "Emri: $emri\nMbiemri: $mbiemri\nEmail: $email\nTel: $telefoni\nMesazhi: $mesazhi";
 
     $headers = "From: $email";
 
@@ -19,3 +20,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 }
 ?>
+
